868aa6393eaaa30565e2c7e8fb2e6dfa2055b470
Joakim Melén <92316614+joakimmelen@users.noreply.github.com>
2022-03-14 16:16:40 +0100

    Updated motion

--- src/partials/AnimatedPage.jsx
+++ src/partials/AnimatedPage.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Container } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
+// const pageMotion = {
+//   initial: { opacity: 0, x: 0 },
+//   animate: { opacity: 1, x: 50, transition: { duration: 2 } },
+//   exit: { opacity: 0, x: 0, transition: { duration: 2 } },
+// };
+
 const pageMotion = {
-  initial: { opacity: 0, x: 0 },
-  animate: { opacity: 1, x: 50, transition: { duration: 2 } },
-  exit: { opacity: 0, x: 0, transition: { duration: 2 } },
-};
+  initial: { opacity: 0, x: -50 },
+  animate: { opacity: 1, x: 0, transition: { duration: 1 } },
+  exit: { opacity: 0, x: -50, transition: { duration: 1 } },
+ };
 
 function AnimatedPage({ children }) {
   return (
